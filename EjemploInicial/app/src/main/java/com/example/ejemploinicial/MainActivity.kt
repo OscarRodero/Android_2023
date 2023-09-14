4ad0dfd576bd695df09d4e8c8a5b266c2c6c48a5
Óscar <oscarrodero@hotmail.com>
2023-09-14 10:07:26 +0200

    Ejemplo con cambio de idioma

--- EjemploInicial/app/src/main/java/com/example/ejemploinicial/MainActivity.kt
+++ EjemploInicial/app/src/main/java/com/example/ejemploinicial/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.ejemploinicial
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var caja = findViewById<TextView>(R.id.txtSaludo)
     }
 }
