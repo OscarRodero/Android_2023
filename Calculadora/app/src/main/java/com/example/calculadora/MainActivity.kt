73e61a0d6e64f4f488972ea2dea98858b5e75431
Óscar <oscarrodero@hotmail.com>
2023-09-15 10:21:32 +0200

    Diseño calculadora creado

--- Calculadora/app/src/main/java/com/example/calculadora/MainActivity.kt
+++ Calculadora/app/src/main/java/com/example/calculadora/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.example.calculadora
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var textoPantalla = findViewById<EditText>(R.id.TextoPantalla)
+        textoPantalla.isEnabled = false
+        textoPantalla.setText("peepo")
     }
 }
