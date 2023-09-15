d590dfdfd4b989f7b38a9da94975034391a3f8a0
Óscar <oscarrodero@hotmail.com>
2023-09-15 08:44:46 +0200

    Controles1

---
+++ Controles1/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Controles1"
+include(":app")
+ 
