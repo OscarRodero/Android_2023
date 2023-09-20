c4feaadee96d825b956659f68340076b4956d054
Óscar <oscarrodero@hotmail.com>
2023-09-20 11:12:28 +0200

    Ejercicio completado y testeado.

--- FormularioRegistro/app/src/main/java/com/example/formularioregistro/MainActivity.kt
+++ FormularioRegistro/app/src/main/java/com/example/formularioregistro/MainActivity.kt
@@ -2,10 +2,34 @@ package com.example.formularioregistro
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.formularioregistro.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnAceptar.setOnClickListener {
+            var listaAsignaturas = mutableListOf<String>()
+            if(binding.chkANDROID.isChecked){
+                listaAsignaturas.add(binding.chkANDROID.text.toString())
+            }
+            if(binding.chkDESARROLLO.isChecked){
+                listaAsignaturas.add(binding.chkDESARROLLO.text.toString())
+            }
+            if(binding.chkSGE.isChecked){
+                listaAsignaturas.add(binding.chkSGE.text.toString())
+            }
+            var alumno = Modelo.Alumno(binding.etxtNombre.text.toString(),
+                binding.etxtApellidos.text.toString(),
+                binding.etxtFechaNacimiento.text.toString(),
+                binding.radioGroup.checkedRadioButtonId.toString(),
+                listaAsignaturas
+                )
+            Toast.makeText(this, alumno.toString(), Toast.LENGTH_LONG).show()
+        }
     }
 }
