1e4707158319eea0bb7225ac265d5e86cb82ddb2
Óscar <oscarrodero@hotmail.com>
2023-09-21 10:18:13 +0200

    Avance de clase

--- LanzarDados/app/src/main/java/com/example/lanzardados/MainActivity.kt
+++ LanzarDados/app/src/main/java/com/example/lanzardados/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.lanzardados
 
-import androidx.appcompat.app.AppCompatActivity
+import android.R
+import android.media.MediaPlayer
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val mediaPlayer: MediaPlayer = MediaPlayer.create(context, R.raw.nombre_del_archivo)
+        mediaPlayer.start() // Para reproducir la música
+
+
     }
 }
