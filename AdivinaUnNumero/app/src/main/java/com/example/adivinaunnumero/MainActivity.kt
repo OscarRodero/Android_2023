0df2a1e264f82fcb9c679a5e107ae48dadbf5751
Óscar <oscarrodero@hotmail.com>
2023-09-22 10:18:18 +0200

    Avances

--- AdivinaUnNumero/app/src/main/java/com/example/adivinaunnumero/MainActivity.kt
+++ AdivinaUnNumero/app/src/main/java/com/example/adivinaunnumero/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.adivinaunnumero
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.adivinaunnumero.databinding.ActivityMainBinding
+import java.util.Random
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        jugar()
+    }
+
+    private fun jugar() {
+        var random = Random()
+        do {
+            var x = random.nextInt(100)
+
+        }while(true)
     }
 }
