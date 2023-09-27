d86ed639267420dc461a0ec65a69be3a4d6f0982
Óscar Rodero <oscarrodero@hotmail.com>
2023-09-27 18:25:52 +0200

    Avances en formulario

---
+++ Encuesta/app/src/main/java/Modelos/Encuesta.kt
@@ -0,0 +1,3 @@
+package Modelos
+
+data class Encuesta(var Nombre:String, var SO:Int, var ListaEspecialidades: ArrayList<String>, var HorasEstudio:Int)
--- Encuesta/app/src/main/java/com/example/encuesta/MainActivity.kt
+++ Encuesta/app/src/main/java/com/example/encuesta/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.encuesta
 
+import Modelos.Encuesta
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.encuesta.databinding.ActivityMainBinding
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
@@ -11,5 +14,33 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.RGSO.check(0)
+        binding.btnValidar.setOnClickListener {
+            var valido = true
+            try {
+                //Genero un tipo encuesta
+                var Espe = ArrayList<String>()
+                var n = Encuesta("",0,Espe,0)
+
+                //Compruebo si el check de anónimo está marcado
+                if(binding.SwAnonimo.isChecked){
+                    n.Nombre="Anonimo"
+                }else{
+                    //Si el check de anónimo no está marcado, reviso que haya texto en el nombre, sino, aviso al usuario.
+                    if(binding.etxtNombre.text.toString()!=""){
+                        n.Nombre=binding.etxtNombre.text.toString()
+                    }else{
+                        Toast.makeText(this,"Debes indicar un nombre o presentarte como anónimo", Toast.LENGTH_SHORT).show()
+                        valido = false
+                    }
+                }
+                n.SO = binding.RGSO.checkedRadioButtonId
+
+            }catch (e:Exception){
+                Toast.makeText(this, e.toString(), Toast.LENGTH_SHORT).show()
+            }
+
+        }
+
     }
 }
