589fecb40a89a0b472f3215bdbbe2612da242a4a
Óscar <oscarrodero@hotmail.com>
2023-09-29 09:14:15 +0200

    Añadidas la segunda activity y la carpeta bien.

--- PrimerRecycleView/app/src/main/java/com/example/primerrecycleview/MainActivity.kt
+++ PrimerRecycleView/app/src/main/java/com/example/primerrecycleview/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.primerrecycleview
 
+import Modelo.Almacen
+import Modelo.FactoriaListaPersonaje
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.primerrecycleview.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        Almacen.personajes = FactoriaListaPersonaje.generaLista(12)
+        Log.e("Fernando", Almacen.personajes.toString())
     }
 }
