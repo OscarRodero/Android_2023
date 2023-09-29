6868a3a7d531d57d0b837105e672b0467f9a8caa
Óscar <oscarrodero@hotmail.com>
2023-09-29 10:18:24 +0200

    Encuestas recibe los datos, el ejemplo de recycleview no funciona.

--- EncuestasBonitas/app/src/main/java/com/example/encuestasbonitas/VentanaResumen.kt
+++ EncuestasBonitas/app/src/main/java/com/example/encuestasbonitas/VentanaResumen.kt
@@ -2,10 +2,16 @@ package com.example.encuestasbonitas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.encuestasbonitas.databinding.ActivityVentanaResumenBinding
 
 class VentanaResumen : AppCompatActivity() {
+    lateinit var binding: ActivityVentanaResumenBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_ventana_resumen)
+        binding = ActivityVentanaResumenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        var Listado = intent.getSerializableExtra("Encuestas")
+
+
     }
 }
--- PrimerRecycleView/app/src/main/java/com/example/primerrecycleview/MainActivity.kt
+++ PrimerRecycleView/app/src/main/java/com/example/primerrecycleview/MainActivity.kt
@@ -1,19 +1,30 @@
 package com.example.primerrecycleview
 
+import Adaptadores.MiAdaptadorRecycler
 import Modelo.Almacen
 import Modelo.FactoriaListaPersonaje
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.primerrecycleview.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding:ActivityMainBinding
+    lateinit var miRecyclerView:RecyclerView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
         Almacen.personajes = FactoriaListaPersonaje.generaLista(12)
         Log.e("Fernando", Almacen.personajes.toString())
+
+        miRecyclerView = binding.RWMiRecycler as RecyclerView
+        miRecyclerView.setHasFixedSize(true)
+        miRecyclerView.layoutManager = LinearLayoutManager(this)
+        var miAdapter = MiAdaptadorRecycler(Almacen.personajes, this)
+        miRecyclerView.adapter = miAdapter
     }
 }
