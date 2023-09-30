0235ed144cf03f162561c4ebcd55b0a394e21859
Óscar Rodero <oscarrodero@hotmail.com>
2023-09-30 12:44:18 +0200

    Auto stash before merge of "EjerciciosDel6Al8" and "origin/EjerciciosDel6Al8"

--- EncuestasBonitas/app/src/main/java/com/example/encuestasbonitas/VentanaResumen.kt
+++ EncuestasBonitas/app/src/main/java/com/example/encuestasbonitas/VentanaResumen.kt
@@ -1,8 +1,10 @@
 package com.example.encuestasbonitas
 
+import Modelos.Encuesta
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.encuestasbonitas.databinding.ActivityVentanaResumenBinding
+import java.io.Serializable
 
 class VentanaResumen : AppCompatActivity() {
     lateinit var binding: ActivityVentanaResumenBinding
@@ -10,8 +12,18 @@ class VentanaResumen : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityVentanaResumenBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        var Listado = intent.getSerializableExtra("Encuestas")
-
-
+        var ListaEncuestas = ArrayList<Encuesta>()
+        ListaEncuestas = intent.getSerializableExtra("Encuestas") as ArrayList<Encuesta>
+        if (ListaEncuestas != null) {
+            val encuestasText = buildString {
+                for (encuesta in ListaEncuestas) {
+                    append(encuesta.toString())
+                    append("\n\n")
+                }
+            }
+            binding.etxtListado.setText(encuestasText)
+        } else {
+            binding.etxtListado.setText("No se encontraron encuestas")
+        }
     }
 }
