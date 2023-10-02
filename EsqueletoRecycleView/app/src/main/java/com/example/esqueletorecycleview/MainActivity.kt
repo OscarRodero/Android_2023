6cb79330627c247df759b2c9b0b480ce70f0808b
Óscar Rodero <oscarrodero@hotmail.com>
2023-10-02 18:44:38 +0200

    Factoria produce usuarios, almacen los recoge, cardview simple añadida.

--- EsqueletoRecycleView/app/src/main/java/com/example/esqueletorecycleview/MainActivity.kt
+++ EsqueletoRecycleView/app/src/main/java/com/example/esqueletorecycleview/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.esqueletorecycleview
 
+import Modelos.Almacen
+import Modelos.FactoriaUsuarios
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.esqueletorecycleview.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        Almacen.usuarios = FactoriaUsuarios.generarUsuarios(10)
+        Log.e("Oscar", Almacen.usuarios.toString())
     }
 }
