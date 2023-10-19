11781fba50dba8dcefb6f4414aa965c7485eef0b
Óscar <oscarrodero@hotmail.com>
2023-10-19 16:37:47 +0200

    Constantes y conexiones añadidas

--- KtorApiRecyclers/conexionktor/src/main/kotlin/Models/Constants.kt
+++ KtorApiRecyclers/conexionktor/src/main/kotlin/Models/Constants.kt
@@ -2,6 +2,11 @@ package Models
 
 object Constants
 {
+    val servidor = "localhost"
+    val puertoDB = 3306
+    val bbdd = "android"
     val ip = "192.168.2.250"
     val port = 8080
+    val dbUser = "root"
+    val dbPwd = ""
 }
---
+++ KtorApiRecyclers/conexionktor/src/main/kotlin/Models/StaticConnection.kt
@@ -0,0 +1,35 @@
+package Models
+
+import java.sql.*
+object StaticConnection {
+    var conexion: Connection? = null
+    var sentenciaSQL: Statement? = null
+    var registros: ResultSet? = null
+    fun abrirConexion(): Int {
+        var cod = 0
+        try {
+            val controlador = "com.mysql.cj.jdbc.Driver"
+            val URL_BD = "jdbc:mysql://" + Constants.servidor+":"+Constants.puertoDB+"/" + Constants.bbdd
+            Class.forName(controlador)
+            conexion = DriverManager.getConnection(URL_BD, Constants.dbUser, Constants.dbPwd)
+            sentenciaSQL = StaticConnection.conexion!!.createStatement()
+            println("Conexion realizada con éxito")
+        } catch (e: Exception) {
+            System.err.println("Exception: " + e.message)
+            cod = -1
+        }
+        return cod
+    }
+
+    // ------------------------------------------------------
+    fun cerrarConexion(): Int {
+        var cod = 0
+        try {
+            conexion!!.close()
+            println("Desconectado de la Base de Datos") // Opcional para seguridad
+        } catch (ex: SQLException) {
+            cod = -1
+        }
+        return cod
+    }
+}
